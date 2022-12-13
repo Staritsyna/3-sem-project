83b98467ddb85586d3f37baee6d20c08edd8e452
Staritsyna <staritsyna.ai@phystech.edu>
2022-12-13 11:54:33 +0300

    Update input.h
    Added function of particle's field

--- input.h
+++ input.h
@@ -9,7 +9,9 @@ float B_x_P(float x, float y, float z, std::string B);
 float B_y_P(float x, float y, float z, std::string B);
 float B_z_P(float x, float y, float z, std::string B);
 
-void full(std::vector<float> co,std::vector<float> vo, std::vector<std::string> B_o ,float m,float q);
-void input();
+void partB(std::vector<float>& B,std::vector<float> c1,std::vector<float> c2,std::vector<float> v,float q);
+void full(int n,  std::vector<std::vector<float>> co, std::vector<std::vector<float>> vo,
+ std::vector<std::string> B_o ,std::vector<float> m,std::vector<float> q);
+void input(int np);
 
 #endif
