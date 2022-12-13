008c36a330b8206a70aa40405c98b57a63ff5021
Staritsyna <staritsyna.ai@phystech.edu>
2022-12-13 13:43:29 +0300

    Update input.h
    Added void which described electrostatic interaction

--- input.h
+++ input.h
@@ -10,6 +10,8 @@ float B_y_P(float x, float y, float z, std::string B);
 float B_z_P(float x, float y, float z, std::string B);
 
 void partB(std::vector<float>& B,std::vector<float> c1,std::vector<float> c2,std::vector<float> v,float q);
+void El(int i,std::vector<float>& a,std::vector<float> c1,std::vector<float> c2,float q1,float q2,float m);
+
 void full(int n,  std::vector<std::vector<float>> co, std::vector<std::vector<float>> vo,
  std::vector<std::string> B_o ,std::vector<float> m,std::vector<float> q);
 void input(int np);
